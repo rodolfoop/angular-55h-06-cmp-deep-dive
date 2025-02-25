09664a57d7ebebe739c4e9598a33bc89b5bca972
rodolfo <rodolfoop@outlook.com>
2025-02-25 13:26:12 -0500

    Using Content Projection and ng-content.

---
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, input, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-item.component.html',
+  styleUrl: './dashboard-item.component.css'
+})
+export class DashboardItemComponent {
+  @Input({required: true}) image!: {source: string, alt: string};
+  // @Input({required: true}) title!: string;
+  
+  // Alternative declaration using signals
+  // image = input.required<{source: string, alt: string}>();
+  title = input.required<string>();
+
+}
