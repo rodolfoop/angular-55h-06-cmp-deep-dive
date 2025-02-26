83154a3dc2096b3cd9ec936ab21dd5462f6416a7
rodolfo <rodolfoop@outlook.com>
2025-02-26 12:00:32 -0500

    Getting access to Template Elements via ViewChild.

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { FormsModule } from '@angular/forms';
@@ -11,12 +11,16 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  @ViewChild('myForm') myFormVar?: ElementRef<HTMLFormElement>;
 
-  onSubmit(title: string, ticketText: string, myForm: HTMLFormElement) {
+
+  onSubmit(title: string, ticketText: string) {
     console.log('New ticket submitted');
     console.log('Title:', title);
     console.log('Text:', ticketText);
-    myForm.reset();
+
+
+    this.myFormVar?.nativeElement.reset();
   }
 
 }
