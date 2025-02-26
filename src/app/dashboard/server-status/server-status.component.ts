be569c0cf6c707fcd1d6b3aed58313a295d9749d
rodolfo <rodolfoop@outlook.com>
2025-02-26 10:07:52 -0500

    Manipulating status with conditional CSS.

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
   imports: [],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+
+  constructor() {
+    setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.4) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.8) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
+
 }
