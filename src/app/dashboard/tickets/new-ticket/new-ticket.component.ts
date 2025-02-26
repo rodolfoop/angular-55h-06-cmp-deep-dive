1c8a65ab3e207ad7f62013f06d28b27295887247
rodolfo <rodolfoop@outlook.com>
2025-02-26 14:01:39 -0500

    Using @ViewChild decorator with onOnInit vs ngAfterViewInit.

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, viewChild } from '@angular/core';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormsModule } from '@angular/forms';
@@ -10,15 +10,24 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {
-  // @ViewChild('myForm') myFormVar?: ElementRef<HTMLFormElement>;
-  private myFormVar = viewChild.required<ElementRef<HTMLFormElement>>('myForm');
+export class NewTicketComponent implements OnInit, AfterViewInit{
+  @ViewChild('myForm') myFormVar?: ElementRef<HTMLFormElement>;
+  // private myFormVar = viewChild.required<ElementRef<HTMLFormElement>>('myForm');
 
   onSubmit(title: string, ticketText: string) {
     console.log('New ticket submitted');
     console.log('Title:', title);
     console.log('Text:', ticketText);
 
-    this.myFormVar().nativeElement.reset();
+    this.myFormVar?.nativeElement.reset();
+  }
+
+  ngOnInit(): void {
+    console.log("ON INIT - this.myFormVar?.nativeElement");
+    console.log(this.myFormVar?.nativeElement); // The element is not available here yet.
+  }
+  ngAfterViewInit(): void {
+    console.log("AFTER VIEW INIT - this.myFormVar?.nativeElement");
+    console.log(this.myFormVar?.nativeElement);
   }
 }
